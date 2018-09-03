0a2bdc8358eee8c2ec436833e3b18c1528bef4bb
dcorn068 <dcorn068@users.noreply.github.com>
2018-09-03 17:16:14 -0400

    initialize navbar

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { AppBar } from 'material-ui';
+
+const NavBar = () => (
+  <AppBar title="Adrianne H. Lee" iconClassNameRight="people" />
+);
+
+export default NavBar;
